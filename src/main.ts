04bd8d9b690af4d1eeb447a44796daa2ec433053
Shup04 <bradleyschmidt04@gmail.com>
2025-12-29 02:49:11 -0800

    Added function to get dosage given desired ppm increase

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,26 @@ const getFraction = (c: Compound, n: Nutrient): number => {
   return f;
 }
 
+const nutrientMgNeeded = (tankLiters: number, targetPpmIncrease: number): number => {
+  if (tankLiters <=0 ) throw new Error("Tank size must be positive");
+  if (targetPpmIncrease <=0 ) throw new Error("Target ppm increase must be positive");
+  return tankLiters * targetPpmIncrease;
+}
+
+const compoundMgNeededForPpm = (tankLiters: number, targetPpmIncrease: number, c: Compound, n: Nutrient): number => {
+  if (tankLiters <=0 ) throw new Error("Tank size must be positive");
+  if (targetPpmIncrease <=0 ) throw new Error("Target ppm increase must be positive");
+  const mgNutrient = nutrientMgNeeded(tankLiters, targetPpmIncrease);
+  const f = getFraction(c, n);
+  return mgNutrient / f; // mg compound needed
+}
+
 const c: Compound = "KNO3";
-const n: Nutrient = "PO4";
+const n: Nutrient = "NO3";
 const f = getFraction(c, n);
 
 console.log(c + " provides " + f + " of " + n + " by mass.");
+
+const mgCompound = compoundMgNeededForPpm(100, 10, "KNO3", "NO3");
+
+console.log("To increase " + n + " by 10 ppm in a 100 liter tank, you need to add " + mgCompound.toFixed(2) + " mg of " + c + ".");
